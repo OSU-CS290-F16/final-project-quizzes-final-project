cb99d5f2637e19a11c9b466481c453887d612d39
Javierili <javieri@oregonstate.edu>
2016-11-29 14:04:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // Hi
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var Handlebars = require('handlebars');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+// Serve static files from public/.
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Listen on the specified port.
+app.listen(port, function () {
+  console.log("== Listening on port", port);
+});
