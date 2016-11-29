2ea23119a3ce75717a8ecb41b6523a0f3c136753
tasniakabir <kabirt@oregonstate.edu>
2016-11-29 14:25:26 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Hi
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
@@ -10,6 +9,9 @@ var port = process.env.PORT || 3000;
 // Serve static files from public/.
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* app.get('*', function (req, res) {
+  res.send('The server is listening!')
+}); */
 
 // Listen on the specified port.
 app.listen(port, function () {
