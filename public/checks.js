f4451af1a296a9fcc1aa0cd15297151c7d9a840a
CaitlinBerger <bergerc@oregonstate.edu>
2016-12-09 14:03:53 -0800

    Created login modal in header file. Updated CSS accordingly
    the text input box may need to be changed to a drop down menu, or just
    have another way to select a user

--- public/checks.js
+++ public/checks.js
@@ -5,7 +5,21 @@
 
 var submit = document.getElementsByClassName('submit-button');
 
-submit[0].addEventListener('click', checkAns);
+submit[0].addEventListener('click', clickSubmit);
+
+function clickSubmit(){
+  console.log('submit button clicked!');
+  var x = document.querySelectorAll('.correct');
+  for (var i = 0; i < x.length; i++){
+    if (x[i].checked == false){
+      //if incorrect answer selected, turn value red by adding css class "turn-red"
+      console.log(x[i], 'turn red');
+      x[i].parentNode.setAttribute('class','red')
+    } else {
+      x[i].parentNode.setAttribute('class','green')
+    }
+  }
+}
 
 function checkAns() {
   console.log('clicked!');
@@ -16,7 +30,6 @@ function checkAns() {
     if (x[i].checked == false){
       //if incorrect answer selected, turn value red by adding css class "turn-red"
       console.log(x[i], 'turn red');
-      //also
     }
   }
 }
