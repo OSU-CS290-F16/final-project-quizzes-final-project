55682e0c755f4b6ae1f4526a4aa747c1e87c436a
Javierili <javieri@oregonstate.edu>
2016-12-09 14:51:34 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var Handlebars = require('handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var quizData = require('./quiz-data');
 
 // Serve static files from public/.
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,8 +17,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Change this depending on what needed 
 app.get('/', function (req, res) {
   res.render('index-page', {
-    title: 'BeavFeed -- Homepage'
+    title: 'BeavFeed -- Homepage',
   });
+});
+
+app.get('/:name', function (req, res, next) {
+
+  var quizData = quizData[req.params.name];
+
+  if (quizData) {
+    res.render('quizPage', {
+      title: 'BeavFeed - ' + quizData.quizName,
+      quizName: quizData.quizName,
+      quizTag: quizData.quizTag,
+      questions: quizData.questions
+    });
+  } else {
+    next();
+  }
+
 });
 
 // Listen on the specified port.
