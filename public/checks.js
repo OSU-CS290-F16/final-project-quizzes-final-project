07c7c704ea927c912a87387220ee4f4069f11467
CaitlinBerger <bergerc@oregonstate.edu>
2016-12-09 09:01:27 -0800

    Working on checking JavaScript

--- public/checks.js
+++ public/checks.js
@@ -3,8 +3,23 @@
 //if answer is incorrect, highlight in red
 //display score at the bottom of the page
 
-var submit = getElementsByClassName('submit-button')
-submit[1].addEventListener('click', 'checkAns')
+var submit = document.getElementsByClassName('submit-button');
+
+submit[0].addEventListener('click', checkAns);
+
+function checkAns() {
+  console.log('clicked!');
+  var x = document.querySelectorAll('.correct');
+  for (var i = 0; i < x.length; i++){
+    //turn correct answer green
+    console.log('turn green');
+    if (x[i].checked == false){
+      //if incorrect answer selected, turn value red by adding css class "turn-red"
+      console.log(x[i], 'turn red');
+      //also
+    }
+  }
+}
 
 function checkAns() {
 
